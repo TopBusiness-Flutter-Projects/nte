c4eb339a3b0ba69aa2d8c4b84a5ddd2f6f134932
Topbuisness <eng>
2023-08-13 12:06:28 +0300

    done

---
+++ lib/core/utils/assets_manager.dart
@@ -0,0 +1,45 @@
+const String imagePath = 'assets/images';
+const String iconsPath = 'assets/icons';
+const String lottiePath = 'assets/lottie';
+
+class ImageAssets {
+  //images
+  static const String logoImage = '$imagePath/logo.png';
+  static const String logoProfileImage = '$imagePath/logo_profile.png';
+  static const String logoIconImage = '$imagePath/logo_icon.png';
+  static const String googleMapImage = '$imagePath/google_maps.jpg';
+  static const String mapImage = '$imagePath/map.png';
+  static const String bottomImage = '$imagePath/bottom_login.png';
+  static const String introBackgroundImage = '$imagePath/intro_background.png';
+  static const String introBackgroundImage2 = '$imagePath/intro_background2.png';
+  static const String splash2 = '$imagePath/splash2.jpeg';
+  static const String bottomCurve2 = '$imagePath/bottom_curve2.png';
+  static const String topStatusBarImage = '$imagePath/top_status_bar.png';
+  static const String postImageIcon = '$imagePath/post.png';
+  static const String profilesImageIcon = '$imagePath/profiles.png';
+  static const String settingImageIcon = '$imagePath/setting.png';
+  static const String callsImageIcon = '$imagePath/calls.png';
+  static const String aboutImageIcon = '$imagePath/about.png';
+  static const String detailsPlaceholder = '$imagePath/details_placeholder.png';
+  static const String privacyImage = '$imagePath/privacy.png';
+  static const String khadamatImage = '$imagePath/khadamat.png';
+  static const String bottomCurveImage = '$imagePath/bottom_curve.png';
+
+  // icons
+  static const String phoneIcon = '$iconsPath/phone.svg';
+  static const String aboutsIcon = '$iconsPath/abouts.svg';
+  static const String callIcon = '$iconsPath/call.svg';
+  static const String myPostsIcon = '$iconsPath/my_posts.svg';
+  static const String postsIcon = '$iconsPath/posts.svg';
+  static const String profileIcon = '$iconsPath/profile.svg';
+  static const String settingIcon = '$iconsPath/settings.svg';
+  static const String aboutIcon = '$iconsPath/about.svg';
+  static const String call2Icon = '$iconsPath/call2.svg';
+  static const String favouriteIcon = '$iconsPath/favourite.svg';
+  static const String shareIcon = '$iconsPath/share.svg';
+
+  //Lottie
+  static const String noItems = '$lottiePath/no_item.json';
+  static const String noExam = '$lottiePath/no_exam.json';
+  static const String noLessons = '$lottiePath/no_lessons.json';
+}
