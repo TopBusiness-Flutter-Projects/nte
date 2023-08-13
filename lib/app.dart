7b7bc868fcf91966153bc1bf08fc072f22ad8444
Topbuisness <eng>
2023-08-13 12:06:28 +0300

    done

---
+++ lib/app.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+
+import 'config/routes/app_routes.dart';
+import 'config/themes/app_theme.dart';
+import 'core/utils/app_strings.dart';
+import 'package:nte/injector.dart' as injector;
+
+class NTE extends StatefulWidget {
+  const NTE({Key? key}) : super(key: key);
+
+  @override
+  State<NTE> createState() => _NTEState();
+}
+
+class _NTEState extends State<NTE> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // print(text);
+
+    // return MultiBlocProvider(
+    //   providers: [
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<SplashCubit>(),
+    //     // ),
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<LoginCubit>(),
+    //     // ),
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<HomeCubit>(),
+    //     // ),
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<PostsCubit>(),
+    //     // ),
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<ProfileCubit>(),
+    //     // ),
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<FavoriteCubit>(),
+    //     // ),
+    //     //
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<PrivacyCubit>(),
+    //     // ),
+    //     //
+    //     //
+    //     // BlocProvider(
+    //     //   create: (_) => injector.serviceLocator<AddServiceCubit>(),
+    //     // ),
+    //
+    //
+    //
+    //   ],
+return GetMaterialApp(
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        theme: appTheme(),
+        themeMode: ThemeMode.light,
+        darkTheme: ThemeData.light(),
+        // standard dark theme
+        localizationsDelegates: context.localizationDelegates,
+        debugShowCheckedModeBanner: false,
+        title: AppStrings.appName,
+        onGenerateRoute: AppRoutes.onGenerateRoute,
+
+    );
+  }
+}
---
+++ lib/config/routes/app_routes.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:nte/features/splash/screens/splash_screen.dart';
+
+import '../../core/utils/app_strings.dart';
+
+class Routes {
+  static const String initialRoute = '/';
+  static const String loginRoute = '/login';
+  static const String homeRoute = '/home';
+  static const String otpRoute = '/otp';
+  static const String notificationDetailsRoute = '/notificationDetails';
+  static const String registerScreenRoute = '/registerScreen';
+  static const String otpScreenRoute = '/otpScreen';
+  static const String verificationScreenRoute = '/verificationScreen';
+  static const String googleMapScreenRoute = '/googleMapScreen';
+  static const String favoriteRoute = '/favorite';
+  static const String fullScreenImageRoute = '/fullScreenImageRoute';
+  static const String editProfileRoute = '/editProfile';
+  static const String allServicesRoute = '/allServices';
+  static const String privacyRoute = '/privacy_about';
+  static const String myPostsRoute = '/my_posts';
+  static const String detailsRoute = '/details';
+  static const String contactUsRoute = '/contact_us';
+  static const String googleMapDetailsRoute = '/google_map_details_screen';
+}
+
+class AppRoutes {
+  static String route = '';
+
+  static Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.initialRoute:
+        return MaterialPageRoute(
+          builder: (context) => const SplashScreen(),
+        );
+
+
+      // case Routes.detailsRoute:
+      //   final service = settings.arguments as ServicesModel;
+      //   return MaterialPageRoute(
+      //     // Extract the service model argument from the settings arguments map
+      //
+      //     builder: (context) => Details(service: service),
+      //   );
+      //
+
+
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+      builder: (context) => const Scaffold(
+        body: Center(
+          child: Text(AppStrings.noRouteFound),
+        ),
+      ),
+    );
+  }
+}
